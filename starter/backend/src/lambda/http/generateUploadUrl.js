f3910c72e48f7364394f1608e207188d358eec2a
Yaser <yaser.al3shry@gmail.com>
2024-12-11 02:57:06 +0200

    image upload with a presigned URL

--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,71 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+
+const s3Client = new S3Client()
+const bucketName = process.env.ATTACHMENTS_S3_BUCKET
+const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
+
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
+const todosTable = process.env.TODOS_TABLE
+
+export async function handler(event) {
+  const todoId = event.pathParameters.todoId
+  const userId = "123"
+  const todo = await getTodo(userId, todoId)
+
+  // TODO raise error if todo does not exist
+
+  // generate upload url
+  const uploadUrl = await getUploadUrl(todoId)
+  const attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${todoId}`
+
+  // update todo with attachment url
+  await dynamoDbClient.put({
+    TableName: todosTable,
+    Item: {
+      ...todo,
+      attachmentUrl
+    }
+  })
+
+  // return a presigned URL to upload a file for a TODO item with the provided id
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      uploadUrl
+    })
+  }
 }
+
+async function getUploadUrl(todoId) {
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: todoId
+  })
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: urlExpiration
+  })
+  return url
+}
+
+async function getTodo(userId, todoId) {
+  const result = await dynamoDbClient.get({
+    TableName: todosTable,
+    Key: {
+      userId,
+      todoId
+    }
+  })
+
+  console.log('Get todo: ', result)
+  return result.Item
+}
+
+
 
